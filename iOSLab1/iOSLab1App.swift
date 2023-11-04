16844589c374d771158e91a5ca56b6c7f3d5c5e1
TheGood77 <thegood77@bk.ru>
2023-11-04 15:25:27 +0600

    Initial Commit

---
+++ iOSLab1/iOSLab1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSLab1App.swift
+//  iOSLab1
+//
+//  Created by TheGood on 04.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOSLab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
